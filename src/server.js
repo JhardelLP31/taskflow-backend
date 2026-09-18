f6e4293715e148448c619ac6bb628590ea6a9e21
JhardelLP31 <jhardel.3129@gmail.com>
2026-09-17 20:22:24 -0500

    Cambios para examen v2

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(morgan(env.nodeEnv === "production" ? "combined" : "dev"));
 
 app.get("/", (req, res) => {
-  res.json({ status: "ok", service: "taskflow-api", version: "1.10.5 - Despliegue automatico para Validar Andes CI/CD" });
+  res.json({ status: "ok", service: "taskflow-api", version: "1.20.5 - Despliegue automatico para Validar Andes  en tiempo REAL CI/CD" });
 });
 app.use("/api", routes);
 app.use(notFound);
